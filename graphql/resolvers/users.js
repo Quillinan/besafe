70a8d1424e309094dbac98204f3d6f5eb5019e92
Tsai <lucastsai123@gmail.com>
2022-02-01 15:50:44 -0300

    validacao de email unico na criacao de usuario

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,12 +1,21 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { UserInputError } = require('apollo-server')
 const { SECRET_KEY } = require('../../config')
 const User = require('../../models/User')
 
 module.exports = {
   Mutation: {
-    async register (_, { registerInput: { email, nome, sobrenome, dataDeNascimento, senha, confirmaSenha } }, context, info) {
+    async register (_, { registerInput: { email, nome, sobrenome, dataDeNascimento, senha, confirmaSenha } }) {
       senha = await bcrypt.hash(senha, 12)
+      const user = await User.findOne({ email })
+      if (user) {
+        throw new UserInputError('email ja existe', {
+          errors: {
+            email: 'email ja existe'
+          }
+        })
+      }
 
       const newUser = new User({
         nome,
